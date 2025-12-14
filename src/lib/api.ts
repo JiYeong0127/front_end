829427bb375885e50107fdadf7853edd7bf2d8e6
JiYeong0127 <zhang6642@gmail.com>
2025-12-14 17:30:25 +0900

    ip 수정

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,7 +5,7 @@
 import axios, { AxiosError } from 'axios';
 import { LoginRequest, LoginResponse, RegisterRequest, RegisterResponse, UsernameExistsResponse, UserProfile } from '../types/auth';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://44.234.58.137';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://inha-rsrs.duckdns.org';
 
 /**
  * API 에러 응답 타입
